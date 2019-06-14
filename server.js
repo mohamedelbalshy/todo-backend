f1c9b47059042e2291d8f4c0c3dfd3f99e9c0422
Mohamed Elbalshy <mohamedbalshy2014@gmail.com>
2019-06-14 04:39:13 +0200

    second

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 
 
-mongoose.connect('mongodb://localhost:27017/todo_backend',{useNewUrlParser: true}, (err)=>{
+mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost:27017/todo_backend',{useNewUrlParser: true}, (err)=>{
     if(err)
         return console.log(err);
     console.log('Connected to mongodb!');
